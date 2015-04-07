b84a0758248336adc4d3ef282fb0b5986f16e483
phoenixMag00 <rees.will@gmail.com>
2015-04-07 16:38:14 -0400

    Minor CSS Changes
    -Images holders now have light grey backgrounds while the image files
    load.
    -Took out useless comments.

--- js/masonry-init-v3-lazy.js
+++ js/masonry-init-v3-lazy.js
@@ -30,15 +30,10 @@ jQuery(document).ready(function ($) {
 
     }).addClass('jmig-img-hide');
 
-    //Remove any gallery column classes from the gallery container. Better for Masonry.
-
-    //$( ".gallery" ).removeClass("gallery-columns-1 gallery-columns-2 gallery-columns-3 gallery-columns-4 gallery-columns-5 gallery-columns-6 gallery-columns-7 gallery-columns-8 gallery-columns-9");
-
     //Lazy load does it thing using the standard masonry init as a function.
 
     $(".gallery-item img").lazyload({
         threshold: 400,
-        placeholder: 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==',
         load: function () {
             $(this).removeClass("jmig-img-hide").addClass("jmig-img-show");
             masonry_update();
